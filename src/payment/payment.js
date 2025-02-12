2a12e0bed5d7bd9d3151a62ebdcbbb49840bb568
mariyatom <mariyatom07@gmail.com>
2025-02-13 12:59:45 +1300

    home button added in the payment page

--- src/payment/payment.js
+++ src/payment/payment.js
@@ -28,8 +28,19 @@ function processPayment(cardNumber, expiryDate, cvv) {
   // Simulate a delay for payment processing
   setTimeout(function () {
     // Normally  use a payment gateway API here (Stripe, PayPal, etc.), need modifications if you integrate gateway
-    document.getElementById('confirmation').innerHTML =
-      '<p>Payment Successful! Thank you for your purchase.</p>'
+    document.getElementById(
+      'confirmation'
+    ).innerHTML = `<p>Payment Successful! Thank you for your purchase.</p>
+      <button id="homeRedirect" style="background-color: orange; color: white; border: none; padding: 8px 16px; cursor: pointer; margin-top: 10px; border-radius: 5px;">
+      Go to Home
+    </button>`
+
+    document
+      .getElementById('homeRedirect')
+      .addEventListener('click', function () {
+        window.location.href = '../../index.html' // Change to the actual home page path
+      })
+
     document.getElementById('confirmation').classList.remove('error')
 
     // Hide payment form and checkout button
